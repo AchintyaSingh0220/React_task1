88ab64bf81702b0cb992435033932469402a8c64
Achintya Singh <113066524+AchintyaSingh0220@users.noreply.github.com>
2023-10-14 11:20:43 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+function WordCounter() {
+  const [state, setState] = React.useState({
+    wordCount: 0, 
+    charCount: 0
+  });
+  
+  const handleKeyPress = (e) => {
+    const count = e.target.value;
+    
+    const countWords = (count) => {
+      if (count.length === 0) {
+        return 0;
+      } else {
+        count = count.replace(/(^\s*)|(\s*$)/gi,"");
+        count = count.replace(/[ ]{2,}/gi," ");
+        count = count.replace(/\n /,"\n");
+        return count.split(' ').length; 
+      }
+    }
+    
+    setState({
+      wordCount: countWords(count),
+      charCount: count.length
+    });
+  }
+  
+  return(
+    <div id='container'>
+      <h1>Responsive Paragraph Word Counter</h1>
+      <textarea 
+        placeholder='Type here'
+        onChange={handleKeyPress}
+        rows='5'
+        cols='55'
+      >    
+      </textarea>
+      <p id='leftalign'> Word Count: <span className='num'> {state.wordCount}</span></p>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WordCounter />
   );
 }
 
